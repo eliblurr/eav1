ad3c8d41abba8a8368e26f9415716b5c9c8c61be
Elvis Eli <segbawuel@aiti-kace.com>
2020-11-29 16:47:16 +0000

    email templates

---
+++ static/email_templates/email_templates.py
@@ -0,0 +1,225 @@
+html = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Demystifying Email Design</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <style type="text/css">
+    a[x-apple-data-detectors] {color: inherit !important;}
+  </style>
+
+</head>
+<body style="margin: 0; padding: 0;">
+  <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+      <td style="padding: 20px 0 30px 0;">
+
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; border: 1px solid #cccccc;">
+  <tr>
+    <td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0;">
+      <img src="https://assets.codepen.io/210284/h1_1.gif" alt="Creating Email Magic." width="300" height="230" style="display: block;" />
+    </td>
+  </tr>
+  <tr>
+    <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+        <tr>
+          <td style="color: #153643; font-family: Arial, sans-serif;">
+            <h1 style="font-size: 24px; margin: 0;">Lorem ipsum dolor sit amet!</h1>
+          </td>
+        </tr>
+        <tr>
+          <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 20px 0 30px 0;">
+            <p style="margin: 0;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+              <tr>
+                <td width="260" valign="top">
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                    <tr>
+                      <td>
+                        <img src="https://assets.codepen.io/210284/left_1.gif" alt="" width="100%" height="140" style="display: block;" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 25px 0 0 0;">
+                        <p style="margin: 0;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+                <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                <td width="260" valign="top">
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                    <tr>
+                      <td>
+                        <img src="https://assets.codepen.io/210284/right_1.gif" alt="" width="100%" height="140" style="display: block;" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 25px 0 0 0;">
+                        <p style="margin: 0;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td bgcolor="#ee4c50" style="padding: 30px 30px;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+        <tr>
+          <td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;">
+            <p style="margin: 0;">&reg; Someone, somewhere 2025<br/>
+           <a href="#" style="color: #ffffff;">Unsubscribe</a> to this newsletter instantly</p>
+          </td>
+          <td align="right">
+            <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
+              <tr>
+                <td>
+                  <a href="http://www.twitter.com/">
+                    <img src="https://assets.codepen.io/210284/tw.gif" alt="Twitter." width="38" height="38" style="display: block;" border="0" />
+                  </a>
+                </td>
+                <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                <td>
+                  <a href="http://www.twitter.com/">
+                    <img src="https://assets.codepen.io/210284/fb.gif" alt="Facebook." width="38" height="38" style="display: block;" border="0" />
+                  </a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+
+"""
+
+html2 = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: burlywood; color: #74787E; height: 100%; hyphens: auto; line-height: 1.4; margin: 0; -moz-hyphens: auto; -ms-word-break: break-all; width: 100% !important; -webkit-hyphens: auto; -webkit-text-size-adjust: none; word-break: break-word;">
+    <style>
+        @media    only screen and (max-width: 600px) {
+            .inner-body {
+                width: 100% !important;
+            }
+
+            .footer {
+                width: 100% !important;
+            }
+        }
+
+        @media    only screen and (max-width: 500px) {
+            .button {
+                width: 100% !important;
+            }
+        }
+    </style>
+     <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+        <tr>
+            <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+                    <tr>
+    <td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;background-color:darkblue">
+    
+        <a href="http://localhost" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: orange; font-size: 20px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 black;">
+            <img style="position:relative;top:-22px;right:-200px " src="http://localhost/img/logo.png" alt="Company Logo">
+        </a>
+    </td>
+</tr>  
+
+                    <!-- Email Body -->
+                    <tr>
+                        <td class="body" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; border-bottom: 1px solid #EDEFF2; border-top: 1px solid #EDEFF2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+                            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
+                                <!-- Body content -->
+                                <tr>
+                                    <td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+                                        <!-- HEADING -->
+                                        <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">
+                                        Hello vvv,
+                                        </h1>
+
+<p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+    Kindly note, a leave with the details below has been cancelled by Janice Gray
+</p>
+
+<table style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-top: 1px solid #EDEFF2; border-bottom: 1px solid #EDEFF2;margin-top: 25px; padding-top: 25px;margin-bottom: 25px; padding-bottom: 25px;" width="100%" cellspacing="0" cellpadding="0">
+		<tbody>
+			<tr>
+			<td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+					<p>NAME</p>
+					<p>LEAVE TYPE</p>
+										<p>APPLIED ON</p>
+					<p>LEAVE START</p>
+					<p>LEAVE END</p>
+					<p>DAYS</p>
+					<p>RELIEVE STAFF</p>
+					<p>COMMENTS</p>
+										<p>ACTION REASON</p>
+								</td>
+			<td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; font-weight:bold">
+				
+					<p>Janice Gray</p>
+					<p>Compassionate Leave</p>
+										<p>Thursday 14th of May 2020 04:01 PM</p>
+					<p>Friday 15th of May 2020</p>
+					<p>Monday 18th of May 2020</p>
+					<p>1</p>
+					<p>Elvis Segbawu</p>
+					<p>ww</p>
+										<p>issue resolved</p>
+								</td>			
+		</tr>
+	</tbody>
+</table>
+
+
+
+
+<p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left; font-weight:bold">
+    Regards,<br>
+</p>
+
+<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-top: 1px solid #EDEFF2; margin-top: 25px; padding-top: 25px;">
+    <tr>
+        
+        <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                    </td>
+    </tr>
+</table>
+
+                                        
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+    <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+"""
