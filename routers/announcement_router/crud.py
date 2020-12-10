e6404c398e6c717728c8178e0a46d96779dad6e0
Elvis Eli <segbawuel@aiti-kace.com>
2020-12-10 20:58:53 +0000

    auth

--- routers/announcement_router/crud.py
+++ routers/announcement_router/crud.py
@@ -1,47 +1,34 @@
-from sqlalchemy import update, and_
-from typing import List
-
-from fastapi import HTTPException
-from sqlalchemy.orm import Session 
-
-from main import get_db
-
-import utils
-
-import sys
-
-from . import models, schemas
-
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
-
 from ..product_router.crud import read_products_by_id
-
 from  sqlalchemy.sql.expression import func
-
+from sqlalchemy import update, and_
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from . import models, schemas
+from typing import List
+from main import get_db
+import utils
+import sys
 
 async def create_announcement(payload:  schemas.CreateAnnouncement, db:Session):
     try:
-
         announcement = models.Announcement(**payload.dict())
         db.add(announcement) 
         db.commit()
         db.refresh(announcement)
         return announcement
-
     except:
         db.rollback()
         print("{}".format(sys.exc_info()))
+        raise HTTPException(status_code=500, detail="Operation failed")
 
 async def update_announcement(id: int, payload: schemas.UpdateAnnouncement, db: Session):
     if not await read_announcement_by_id(id, db):
         raise HTTPException( status_code=404)
-
-    try:
-        
+    try: 
         db.query(models.Announcement).filter(models.Announcement.id == id).update(payload.dict(exclude_unset=True))
         db.commit()
         return await read_announcement_by_id(id, db)
-
     except:
         db.rollback()
         print("{}".format(sys.exc_info()))
--- routers/announcement_router/main.py
+++ routers/announcement_router/main.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Response
-from sqlalchemy.orm import Session
-from . import crud, schemas,models
-from typing import List, Optional
-from pydantic import UUID4, EmailStr
-import jwt
-from datetime import timedelta
-import sys
-
 from main import get_db, oauth2_scheme
+from pydantic import UUID4, EmailStr
+from . import crud, schemas,models
+from sqlalchemy.orm import Session
+from typing import List, Optional
+from datetime import timedelta
 import utils
+import sys
+import jwt
 
 access_token_expires = timedelta(minutes=30)
 
@@ -32,13 +31,13 @@ async def delete_announcement(id: int, db: Session = Depends(get_db)):
 async def read_announcement(search:str=None, value:str=None, db: Session = Depends(get_db)):
     return await crud.read_announcement(search, value, db)
 
-@router.get("/{id}", description="read announcement", response_model=schemas.Announcement)
+@router.get("/{id}", description="read announcement by id", response_model=schemas.Announcement)
 async def read_announcement_by_id(id: int, db: Session = Depends(get_db)):
     announcement = await crud.read_announcement_by_id(id, db)
     if announcement is None:
         raise HTTPException(status_code=404)
     return announcement
 
-@router.get("/randomize/announcement", description="read random announcement")
+@router.get("/random/announcement", description="read random announcement")
 async def read_random_announcement(db: Session = Depends(get_db)):
     return await crud.read_random_announcement(db)
